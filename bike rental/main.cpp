827254a9666d75d8f02c20fd443c4397949cd847
prasnakumar <prasnakumar.nayaka@gmail.com>
2020-05-06 13:27:11 +0530

    display of bikes

--- bike rental/main.cpp
+++ bike rental/main.cpp
@@ -5,13 +5,12 @@ using namespace std;
 char bike[20]="bike.txt",rents[20]="rents.txt";
 struct record
 {
-    char bike[50],brand_name[50],mileage[40],rating[10];
+    char bike[50],brand_name[50],mileage[40],rents[10];
 };
 class a{
 public:void pack(record r);
-    void unpack(record r);
+    record unpack(char a[]);
     void bikes();
-    void rents();
     void addbikes();};
 
 void a::pack(record r)
@@ -24,41 +23,73 @@ strcat(buff,r.brand_name);
 strcat(buff,"|");
 strcat(buff,r.mileage);
 strcat(buff,"|");
-strcat(buff,r.rating);
+strcat(buff,r.rents);
 strcat(buff,"|");
 for(int i=0;i<70-strlen(buff);i++)
     strcat(buff,"|");
 fp<<buff<<endl;
 fp.close();
 }
+record a::unpack(char buff[])
+{
+    record r;
+    int i=0,j=0;
+    while(buff[j]!='|')
+        r.bike[i++]=buff[j++];
+    r.bike[i]='\0';
+    i=0;
+    j++;
+  while(buff[j]!='|')
+        r.brand_name[i++]=buff[j++];
+    r.brand_name[i]='\0';
+    i=0;
+    j++;
+      while(buff[j]!='|')
+        r.mileage[i++]=buff[j++];
+    r.mileage[i]='\0';
+    i=0;
+    j++;
+       while(buff[j]!='|')
+        r.rents[i++]=buff[j++];
+    r.rents[i]='\0';
+    return(r);
+    }
 void a::addbikes(){
-
 record r;
 cout<<"enter the bike\n";
 cin>>r.bike;
-cout<<"enter the brand name\n";
+cout<<"enter the model\n";
 cin>>r.brand_name;
 cout<<"enter the mileage of bike \n";
 cin>>r.mileage;
-cout<<"rate the condition of the bike out of 5\n";
-cin>>r.rating;
+cout<<"cost for per hour of usage\n";
+cin>>r.rents;
 pack(r);
 }
 void a::bikes(){
-cout<<"the bikes are here\n";
+cout<<"all the bikes";
+fstream fp;
+char buff[45];
+record r;
+fp.open(bike,ios::in);
+cout<<"\n#\tbike\tmodel\tmileage\trent cost per hr\n";
+int c=1;
+while(1)
+{fp.getline(buff,70);
+    if(fp.eof())break;
+    r=unpack(buff);
+cout<<c++<<"\t"<<r.bike<<"\t"<<r.brand_name<<"\t"<<r.mileage<<"\t"<<r.rents<<endl;
 
 }
-void a::rents(){
-cout<<"the rents are\n ";
+fp.close();
 }
-
 int main()
 {char number;
 a b;
 
     cout<<"1. BIKES";
     cout<<"2. add bikes";
-    cout<<"3. rents";
+    cout<<"3. display all bikes";
     cout<<"4. Exit";
 
     while('4'){
@@ -71,7 +102,7 @@ case '2':
     b.addbikes();
     break;
 case '3':
-    b.rents();
+    b.bikes();
     break;
 
 case '4':
